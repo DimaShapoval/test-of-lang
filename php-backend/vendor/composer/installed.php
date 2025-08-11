35a55d067111f925ecbf247125a16d92ff8c6878
DimaShapoval <dsapoval422@gmail.com>
2025-08-11 13:48:25 +0300

    php

---
+++ php-backend/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'myvendor/myproject',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2c730b052af4a8e2e1203714c11e52c467ed117d',
+        'type' => 'composer-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'myvendor/myproject' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2c730b052af4a8e2e1203714c11e52c467ed117d',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
